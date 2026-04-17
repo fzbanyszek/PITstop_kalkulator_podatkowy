beb0f3c891ff46ae2c7a01e50439df85890aeb59
KarolinaRatajczyk <s31992@pjwstk.edu.pl>
2026-04-17 21:58:40 +0100

    page footer on every page

--- pages/home.py
+++ pages/home.py
@@ -14,3 +14,13 @@ Ta aplikacja pozwala:
 """)
 
 st.info("Wybierz stronę z menu po lewej stronie.")
+
+st.markdown("---")
+st.markdown(
+    """
+    <div style='text-align: center; color: #808495; font-size: 0.9em;'>
+    Developed by <b>Karolina Ratajczyk</b> and <b>Filip Zbanyszek</b>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
--- pages/ustawienia.py
+++ pages/ustawienia.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.title("Ustawienia")
 st.info("Ta sekcja jest na razie pusta.")
+
+st.markdown("---")
+st.markdown(
+    """
+    <div style='text-align: center; color: #808495; font-size: 0.9em;'>
+    Developed by <b>Karolina Ratajczyk</b> and <b>Filip Zbanyszek</b>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
