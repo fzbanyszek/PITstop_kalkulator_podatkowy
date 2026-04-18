95218a8873ed6a7d9ab568fd912cc18ae767d7c0
Filip <filipz212121@gmail.com>
2026-04-18 19:48:17 +0200

    translation files

---
+++ translations/common.py
@@ -0,0 +1,16 @@
+TRANSLATIONS = {
+    "en": {
+        "app_title": "PITstop",
+        "page_home": "Home",
+        "page_calculator": "Tax calculator",
+        "page_settings": "Settings",
+        "language_label": "Language",
+    },
+    "pl": {
+        "app_title": "PITstop",
+        "page_home": "Strona tytułowa",
+        "page_calculator": "Kalkulator podatkowy",
+        "page_settings": "Ustawienia",
+        "language_label": "Język",
+    },
+}
